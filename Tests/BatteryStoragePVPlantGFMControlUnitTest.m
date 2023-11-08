ecbadf2d92fe55d47534040233bbca4110491c9a
Rajarshi Dutta <rajarshd@mathworks.com>
2023-11-08 18:28:40 +0530

    Updated CICD test

---
+++ Tests/BatteryStoragePVPlantGFMControlUnitTest.m
@@ -0,0 +1,44 @@
+classdef BatteryStoragePVPlantGFMControlUnitTest < matlab.unittest.TestCase
+    %% Class implementation of unit test
+    % Copyright 2023 The MathWorks, Inc.
+
+    properties
+        modelname = "BatteryStoragePVPlantGFM";
+        openfigureListBefore
+    end
+
+    methods(TestMethodSetup)
+
+        function listOpenFigures(testCase)
+            % List all open figures
+            testCase.openfigureListBefore = findall(0,'Type','Figure');
+        end
+    end
+
+    methods(TestMethodTeardown)
+
+        function closeOpenedFigures(testCase)
+            % Close all figure opened during test
+            figureListAfter = findall(0,'Type','Figure');
+            figuresOpenedByTest = setdiff(figureListAfter, testCase.openfigureListBefore);
+            arrayfun(@close, figuresOpenedByTest);
+        end
+    end
+
+    methods (Test)
+        function SolarGFMControl_Test_1(testCase)
+            BatteryStoragePVPlantGFMParameters
+            load_system(testCase.modelname)
+            testCase.addTeardown(@()close_system(testCase.modelname, 0));
+            set_param(testCase.modelname,StopTime = "0.5")
+            sim(testCase.modelname)
+        end
+
+        function WindFarmGFMControl_Test_2(testCase)
+            load_system(testCase.modelname)
+            testCase.addTeardown(@()close_system(testCase.modelname, 0));
+            set_param(testCase.modelname,StopTime = "0.5")
+            BatteryStoragePVPlantGFMMainPage
+        end
+    end % methods (Test)
+end
---
+++ Tests/testrunnerPVPlantBESSGFMControl.m
@@ -0,0 +1,46 @@
+%% Script to run unit tests
+% This script runs tests for component-level and system-level tests.
+% Note that tests for detailed model applications are not run
+% to avoid long-running tests.
+
+% Copyright 2023 The MathWorks, Inc.
+
+relStr = matlabRelease().Release;
+disp("This is MATLAB " + relStr + ".")
+
+topFolder = currentProject().RootFolder;
+
+%% Create test suite
+
+suite = matlab.unittest.TestSuite.fromFile(...
+fullfile(topFolder, "Tests", "BatteryStoragePVPlantGFMControlUnitTest.m"));
+
+%% Create test runner
+
+runner = matlab.unittest.TestRunner.withTextOutput( ...
+  OutputDetail = matlab.unittest.Verbosity.Detailed);
+%% MATLAB Code Coverage Report
+
+coverageReportFolder = fullfile(topFolder, "coverage" + relStr);
+if not(isfolder(coverageReportFolder))
+  mkdir(coverageReportFolder)
+end
+
+coverageReport = matlab.unittest.plugins.codecoverage.CoverageReport( ...
+  coverageReportFolder, ...
+  MainFile = "PV Plant and BESS with GFM Control Coverage" + relStr + ".html" );
+
+plugin = matlab.unittest.plugins.CodeCoveragePlugin.forFile( ...
+  [ ...
+  fullfile(topFolder, "ScriptsData", "BatteryStoragePVPlantGFMMainPage.mlx")
+  fullfile(topFolder, "ScriptsData", "BatteryStoragePVPlantGFMComparisonGFLvsGFM.m")
+  fullfile(topFolder, "ScriptsData","BatteryStoragePVPlantGFMParameters.m")          
+    ], ...
+  Producing = coverageReport );
+
+addPlugin(runner, plugin)
+
+%% Run tests
+results = run(runner, suite);
+out = assertSuccess(results);
+disp(out);
