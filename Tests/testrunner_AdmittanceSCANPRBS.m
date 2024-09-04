76c265b7b92dd4afaf720d07b86132dd92d06beb
Rajarshi Dutta <rajarshd@mathworks.com>
2024-09-04 12:23:06 +0530

    Updated tests for R24a

---
+++ Tests/testrunner_AdmittanceSCANPRBS.m
@@ -0,0 +1,33 @@
+%% Script to run unit tests
+% This script runs tests for component-level and system-level tests.
+% Note that tests for detailed model applications are not run
+% to avoid long-running tests.
+% Copyright 2024 The MathWorks, Inc.
+relStr = matlabRelease().Release;
+disp("This is MATLAB " + relStr + ".")
+topFolder = currentProject().RootFolder;
+%% Create test suite
+suite = matlab.unittest.TestSuite.fromFile(...
+    fullfile(topFolder, "Tests", "AdmittanceScanPRBSUnitTest.m"));
+%% Create test runner
+runner = matlab.unittest.TestRunner.withTextOutput( ...
+    OutputDetail = matlab.unittest.Verbosity.Detailed);
+%% MATLAB Code Coverage Report
+coverageReportFolder = fullfile(topFolder, "coverage" + relStr);
+if not(isfolder(coverageReportFolder))
+    mkdir(coverageReportFolder)
+end
+coverageReport = matlab.unittest.plugins.codecoverage.CoverageReport( ...
+    coverageReportFolder, ...
+    MainFile = "Admittance Scan Test Coverage" + relStr + ".html" );
+plugin = matlab.unittest.plugins.CodeCoveragePlugin.forFile(...
+    [...
+    fullfile(topFolder, "ScriptsData", "TestHarnessDescription.mlx")...
+    fullfile(topFolder, "ScriptsData", "admittancescanPRBS.m")], ...
+    Producing = coverageReport );
+addPlugin(runner, plugin)
+%% Run tests
+results = run(runner, suite);
+out = assertSuccess(results);
+disp(out);
+ 
